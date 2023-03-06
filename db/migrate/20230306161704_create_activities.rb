9b0f756fb24a8d42664df3852abb96ee18d47bd3
Hugo ROBERT <hugo.pierre.robert@gmail.com>
2023-03-06 17:43:33 +0100

    creation des models

---
+++ db/migrate/20230306161704_create_activities.rb
@@ -0,0 +1,20 @@
+class CreateActivities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :activities do |t|
+      t.string :title
+      t.string :description
+      t.string :price
+      t.integer :duration
+      t.string :address
+      t.string :ages
+      t.string :photos
+      t.string :category
+      t.string :status
+      t.boolean :done
+      t.date :date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
