026bf32780c03f817b8ff825e556b165d5c1e813
Camille Jaumier <camille.jaumier@icloud.com>
2023-03-08 14:43:07 +0100

    showww

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,22 @@
 class ActivitiesController < ApplicationController
 
+  before_action :set_activity, only: %i[show]
+
   def index
     @activities = Activity.all
   end
 
   def show
     @activity = Activity.find(params[:id])
+  end
+
+  private
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def activity_params
+    params.require(:activity).permit(:title, :description, :price, :duration, :address, :ages, :category, :status)
   end
 end
