72aff8aea35c1c01b2ba9eccde67969b41b278ba
Camille Jaumier <camille.jaumier@icloud.com>
2023-03-09 13:04:10 +0100

    filter for categories

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,13 @@
 class ActivitiesController < ApplicationController
 
   def index
-    if params[:city].present?
-      @activities = Activity.near(params[:city], params[:km])
+    if params[:activity][:city].present?
+      @activities = Activity.near(params[:activity][:city], params[:activity][:km])
     else
       @activities = Activity.all
+    end
+    if params[:activity][:category].compact_blank.present?
+      @activities = @activities.where(category: params[:activity][:category].compact_blank)
     end
   end
 
