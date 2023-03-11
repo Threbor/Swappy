e8b9fa7cdd23aefb11fc65633f7f981a030fce8f
Hugo ROBERT <hugo.pierre.robert@gmail.com>
2023-03-11 20:00:56 +0100

    ajout de la page profile avec les activités DONE. Ajout btn pour marquer une activité faite. lien des btn pour crer un GROUP

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,8 @@
 class GroupsController < ApplicationController
 
   def index
-    @groups = Group.all
-    @group_users = GroupUser.all
-    @current_user_groups = GroupUser.where(user: current_user)
+    @groups = Group.all.sort.reverse
+
   end
 
   def show
@@ -11,4 +10,54 @@ class GroupsController < ApplicationController
     @message = Message.new
   end
 
+  def new
+    @activity = Activity.find(params["activity_id"])
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save && !@group.done
+      redirect_to @group
+    elsif @group.save && @group.done
+      redirect_to groups_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+
+    if @group.update(group_params) && !@group.done
+      redirect_to @group
+
+    elsif @group.update(group_params) && @group.done
+      redirect_to groups_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_group
+
+  end
+
+  def group_params
+    params.require(:group).permit(:activity_id, :name, :done)
+
+  end
 end
