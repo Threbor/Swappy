0a0b7d43738e067d15a454a730fe4acc9ad08721
helene mercerolle <helenepro@laposte.net>
2023-03-14 09:49:51 +0100

    changement de la home-page / modification des routes dans les favorites et icones / nouvelle route crée

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_11_142921) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_152705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_11_142921) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "done", default: false
+    t.date "date"
     t.index ["activity_id"], name: "index_groups_on_activity_id"
   end
 
